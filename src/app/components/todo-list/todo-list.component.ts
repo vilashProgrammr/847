d1c3ff394e31dac7e2458deddfc9e9213a6fbb3a
Usairim Isani <usairim.isani@outlook.com>
2017-08-02 21:04:06 +0500

    Todo App with ngmodel now able to clear input field

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -9,25 +9,24 @@ import {
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-  todoList: Object[] = [{
-    item: "Call Mama",
-    isComplete: false
-  }];
-
+  todoList: Object[] = [
+  //   {
+  //   item: "Call Mama",
+  //   isComplete: false
+  // }
+];
+  todoInput:String='';
   constructor() {}
 
   ngOnInit() {}
-
-  addTodo(todo) {
-
+  addTodoInput(){
     this.todoList.push({
-      item: todo,
+      item: this.todoInput,
       isComplete: false
     });
-    
-
-
+    this.todoInput = '';
   }
+
   deleteTodo(item) {
     this.todoList = this.todoList.filter((todo: {
       item: string,
